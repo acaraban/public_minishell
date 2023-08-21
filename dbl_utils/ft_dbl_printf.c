e9e551178d5edc4556ee649c1a78d0d7592c84d7
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-08-21 15:55:18 +0200

    organizo los ficheros y subo la funcion custom env

--- dbl_utils/ft_dbl_printf.c
+++ dbl_utils/ft_dbl_printf.c
@@ -1,5 +1,13 @@
 #include "../minishell.h"
 
+/*
+	This function is to print a double pointer.
+	txt: 	text before the array item
+	ftxt:	text after
+	sal:	0 -> ?
+			1 -> ? 
+*/
+
 void	ft_dbl_printf(char *txt, char **arr, char *ftxt, int sal)
 {
 	int i;
