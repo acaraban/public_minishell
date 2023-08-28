431037d20146ab819c3519385e69d4daf8fc308a
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-08-28 10:15:45 +0200

    tested environment vars with this strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,45 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: acaraban <acaraban@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/18 18:30:23 by acaraban          #+#    #+#             */
-/*   Updated: 2022/06/24 15:38:26 by acaraban         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
+
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	s1_len;
-	size_t	s2_len;
-	size_t	stot_len;
-	char	*rtn;
+	int		len_s1;
+	int		i;
+	char	*s3;
 
-	if (!s1 && !s2)
-		return (ft_strdup(""));
-	if (s1 && !s2)
-		return (ft_strdup(s1));
-	if (!s1 && s2)
-		return (ft_strdup(s2));
-	s1_len = ft_strlen((char *)s1);
-	s2_len = ft_strlen(s2);
-	stot_len = s1_len + s2_len + 1;
-	rtn = malloc(sizeof(char) * stot_len);
-	if (!rtn)
-		return (0);
-	ft_memmove(rtn, s1, s1_len);
-	ft_memmove(rtn + s1_len, s2, s2_len);
-	rtn[stot_len - 1] = '\0';
-	return (rtn);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	s3 = (char *)malloc((len_s1 + ft_strlen(s2) + 1) * sizeof(char));
+	if (s3 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		s3[len_s1 + i] = s2[i];
+		i++;
+	}
+	s3[len_s1 + i] = '\0';
+	return (s3);
 }
-
-// int main(void)
-// {
-// 	char ui[] = "adsfjasdffgsgfar";
-// 	char sd[] = "rwett";
-// 	printf("%s", ft_strjoin(ui, sd));
-// }
