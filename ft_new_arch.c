7b61d92fdde27079a25d892d65e9942c34d126a3
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-05 12:52:19 +0200

    mensaje de error mejorado

--- ft_new_arch.c
+++ ft_new_arch.c
@@ -3,7 +3,7 @@
 /*
     nombrearchivo --> name of the file to create
     If there are not rights to write on the file, this is error msg:
-    e.g. permission denied: <filename>
+    e.g. bash: <filename>: Permission denied
 */
 
 int new_arch(char *name)
@@ -14,9 +14,10 @@ int new_arch(char *name)
 	nombrearchivo = ft_strdup(name);
     descriptorarchivo = open(nombrearchivo, O_WRONLY | O_CREAT, 0644);
     if (descriptorarchivo == -1) {
+        ft_putstr_fd("minishell: ", 2);
         perror(nombrearchivo);
-        return 0;
+        return (0);
     }
     close(descriptorarchivo);
-    return 1;
+    return (1);
 }
