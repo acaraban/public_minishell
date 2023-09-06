eee5d85ffd35895777324e64ead012c707334f72
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-06 17:22:45 +0200

    separo en funciones pequeñas la minishell

--- ft_minishell.c
+++ ft_minishell.c
@@ -16,41 +16,9 @@ void	handle_sigint(int sig)
 		rl_on_new_line();
 	}
 }
-/*
-	ft_delete_env_oldpwd()
-	This function searches for the OLDPWD environment var in the minishell program 
-	(not the mac) and deletes it.
-*/
 
-int main(int argc, char **argv, char **env)
+void ft_minishell_bucle(int boo, char *txt, t_global *glb)
 {
-	//atexit(memleaks);
-	char *txt;
-	t_global *glb;
-	int boo;
-	int i;
-	int j;
-
-	(void)argc;
-	(void)argv;
-	j = 0;
-	glb = (t_global *)calloc(sizeof(t_content), 1);
-	signal(SIGINT, handle_sigint);
-	signal(SIGQUIT, SIG_IGN);
-	glb[0].err_stat = 0;
-	glb[0].new_stat = 0;
-	boo = 1;
-	i = 0;
-	while (env[i])
-		i++;
-	glb->env = (char **)calloc(sizeof(char *), i + 1);
-	glb->env[i] = NULL;
-	while (j < i)
-	{
-		glb[0].env[j] = strdup(env[j]);
-		j++;
-	}
-	ft_delete_env_oldpwd(glb->env);
 	while (boo)
 	{
 		txt = readline("minishell>");
@@ -68,4 +36,52 @@ int main(int argc, char **argv, char **env)
 		glb[0].new_stat = glb[0].err_stat;
 		glb[0].err_stat = 0;
 	}
+}
+
+/*
+	Function to save a copy of all environment vars in our struct.
+*/
+
+void ft_dup_envs(t_global *glb, int i, char **env)
+{
+	int j;
+
+	j = 0;
+	while (j < i)
+	{
+		glb[0].env[j] = strdup(env[j]);
+		j++;
+	}
+}
+/*
+	ft_delete_env_oldpwd()
+	This function searches for the OLDPWD environment var in the minishell program 
+	(not the mac) and deletes it.
+*/
+
+int main(int argc, char **argv, char **env)
+{
+	//atexit(memleaks);
+	char *txt;
+	t_global *glb;
+	int boo;
+	int i;
+
+	(void)argc; // init vars
+	(void)argv;
+	glb = (t_global *)calloc(sizeof(t_content), 1);
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
+	glb[0].err_stat = 0;
+	glb[0].new_stat = 0;
+	boo = 1;
+	i = 0;
+	while (env[i])
+		i++;
+	glb->env = (char **)calloc(sizeof(char *), i + 1);
+	glb->env[i] = NULL;
+	ft_dup_envs(glb, i, env);
+	ft_delete_env_oldpwd(glb->env);
+	txt = NULL;
+	ft_minishell_bucle(boo, txt, glb);
 }
