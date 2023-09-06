c71f4006c19c909f8fab90cb9e243bcca0bb6510
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-06 17:32:44 +0200

    reduciendo lineas

--- ft_minishell.c
+++ ft_minishell.c
@@ -53,6 +53,16 @@ void ft_dup_envs(t_global *glb, int i, char **env)
 		j++;
 	}
 }
+
+void ft_init_vars(int argc, char **argv, t_global *glb)
+{
+	(void)argc;
+	(void)argv;
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
+	glb[0].err_stat = 0;
+	glb[0].new_stat = 0;
+}
 /*
 	ft_delete_env_oldpwd()
 	This function searches for the OLDPWD environment var in the minishell program 
@@ -67,13 +77,8 @@ int main(int argc, char **argv, char **env)
 	int boo;
 	int i;
 
-	(void)argc; // init vars
-	(void)argv;
-	glb = (t_global *)calloc(sizeof(t_content), 1);
-	signal(SIGINT, handle_sigint);
-	signal(SIGQUIT, SIG_IGN);
-	glb[0].err_stat = 0;
-	glb[0].new_stat = 0;
+	glb = (t_global *)ft_calloc(sizeof(t_content), 1);
+	ft_init_vars(argc, argv, glb);
 	boo = 1;
 	i = 0;
 	while (env[i])
