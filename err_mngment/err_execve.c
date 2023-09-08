d7b41dfc3d6ecb7436fc01f91a83e1079261216a
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-08 15:48:05 +0200

    norminette

--- err_mngment/err_execve.c
+++ err_mngment/err_execve.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   err_execve.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msintas- <msintas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 15:38:21 by msintas-          #+#    #+#             */
+/*   Updated: 2023/09/08 15:45:49 by msintas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minishell.h"
 
 /*
@@ -7,7 +19,8 @@
     The messages should be printed in the error output, and not stdout
     otherwise gets redirected with dup2 to the pipe and nothing is printed
     in the console.
-    errno 14 -- Bad address, instead should print bash: foo: command not found
+    errno 14 EFAULT -- Bad address, instead should print 
+						e.g. bash: foo: command not found
     errno 2 ENOENT -- bash: cd: kfjasldja: No such file or directory
                       e.g. wc -l < input.txt
     errno 13 EACCES -- Permission denied
@@ -16,32 +29,31 @@
                             ./a.out file_without_permissions ---> 126
 */
 
-void handle_execve_error_message(t_content *cont, int i)
+void	handle_execve_error_message(t_content *cont, int i)
 {
-    if (errno == EFAULT) //14
-    {
-        ft_putstr_fd("minishell: ", 2);
-        ft_putstr_fd(ft_strjoin(cont[i].cmd, ": command not found\n"), 2);
+	if (errno == EFAULT)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(ft_strjoin(cont[i].cmd, ": command not found\n"), 2);
 		exit(127);
-    }
-    else if (errno == ENOENT) // 2
-    {
-        ft_putstr_fd("minishell: ", 2);
-        perror(cont[i].infile);
-        exit(1);
-    }
-    else if (errno == EACCES) 
-    {
-        ft_putstr_fd("minishell: ", 2);
-        if (cont[i].infile)
-        {
-            perror(cont[i].infile);
-            exit(1);
-        }
-        else
-        {
-            perror(cont[i].cmd);
-            exit(126);
-        }
-    }
+	}
+	else if (errno == ENOENT)
+	{
+		perror(ft_strjoin("minishell: ", cont[i].infile));
+		exit(1);
+	}
+	else if (errno == EACCES)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		if (cont[i].infile)
+		{
+			perror(cont[i].infile);
+			exit(1);
+		}
+		else
+		{
+			perror(cont[i].cmd);
+			exit(126);
+		}
+	}
 }
--- err_mngment/err_stx.c
+++ err_mngment/err_stx.c
@@ -1,6 +1,6 @@
 #include "../minishell.h"
 
-void err_stx(char *txt, t_content *cont)
+void	err_stx(char *txt, t_content *cont)
 {
 	ft_printf("%s", txt);
 	cont->global->err_stat = STX_ERR;
