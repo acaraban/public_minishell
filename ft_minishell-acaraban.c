e567e5ed90e2e09b23c27e0aa85c8cc5162f4ced
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-09 23:46:56 +0200

    modifico ft_minishell.c

--- ft_minishell-acaraban.c
+++ ft_minishell-acaraban.c
@@ -3,6 +3,12 @@
 #include <readline/history.h>
 
 int err_status = 0;
+
+void memleaks(void)
+{
+	system("leaks -q minishell");
+}
+
 void	handle_sigint(int sig)
 {
 	if (sig == 2)
@@ -14,9 +20,19 @@ void	handle_sigint(int sig)
 	}
 }
 
-void	ft_minishell_bucle(int boo, char *txt, t_global *glb)
+int main(int argc, char **argv, char **env)
 {
-	while (boo)
+	//atexit(memleaks);
+	char *txt;
+	t_global *glb;
+	int boo;
+
+	(void)argc;
+	(void)argv;
+	glb = NULL;
+	glb = ft_init(glb, env, err_status);
+	boo = 1;
+	while (boo) // desde aqui hecho
 	{
 		glb->new_stat = err_status;
 		err_status = 0;
@@ -28,33 +44,13 @@ void	ft_minishell_bucle(int boo, char *txt, t_global *glb)
 		}
 		else
 		{
-			free(txt);
+			free (txt);
 			free_dbl(glb->env);
 			free (glb);
 			ft_printf("exit\n");
 			boo = 0;
 		}
 		if (boo == 1)
-			ft_nrmntt_1(glb);
+			ft_nrmntt_1(glb); // hasta aqui hecho
 	}
-}
-
-/*
-	ft_delete_env_oldpwd()
-	This function searches for the OLDPWD environment var in the minishell program 
-	(not the mac) and deletes it.
-*/
-
-int	main(int argc, char **argv, char **env)
-{
-	char		*txt;
-	t_global	*glb;
-	int			boo;
-
-	(void)argc;
-	(void)argv;
-	glb = NULL;
-	glb = ft_init(glb, env, err_status);
-	boo = 1;
-	ft_minishell_bucle(boo, txt, glb);
 }
