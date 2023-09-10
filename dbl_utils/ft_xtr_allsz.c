dce2faa0b816ad6f44f4637daa489255a2fe3d01
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-10 20:13:51 +0200

    separo funciones

--- dbl_utils/ft_xtr_allsz.c
+++ dbl_utils/ft_xtr_allsz.c
@@ -11,6 +11,26 @@ void free_dbl(char **new)
 	free(new);
 }
 
+void init_xtrallsz_values(int *i, int *h, int *l, int *k)
+{
+	i = 0;
+	h = 0;
+	l = 0;
+	k = 0;
+}
+
+int set_i_value(int *i, char **arr)
+{
+	int n;
+
+	n = *i;
+	while (arr[n] )
+	{
+		i++;
+	}
+	return (n);
+}
+
 char **ft_xtr_allsz_free(char **arr, char **add, int posadd)
 {
 	char **jj;
@@ -19,12 +39,8 @@ char **ft_xtr_allsz_free(char **arr, char **add, int posadd)
 	int l;
 	int k;
 
-	i = 0;
-	h = 0;
-	l = 0;
-	k = 0;
-	while (arr[i] )
-		i++;
+	init_xtrallsz_values(&i, &h, &l, &k);
+	i = set_i_value(&i, arr);
 	while (add[h + posadd])
 		h++;
 	jj = (char **)ft_calloc(sizeof(char *), i + h + 1);
@@ -32,9 +48,7 @@ char **ft_xtr_allsz_free(char **arr, char **add, int posadd)
 	while (l < h + i)
 	{
 		if (l < i)
-		{
 			jj[l] = ft_strdup(arr[l]);
-		}
 		else
 		{
 			jj[l] = ft_strdup(add[k + posadd]);
