aba68129fdd51cab2205c62e2f2ede1a0766f13c
acaraban <acaraban@student.42malaga.com>
2023-09-15 13:28:32 +0200

    norm extra_utils heredoc heredoc_more

--- ft_heredoc.c
+++ ft_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: acaraban <acaraban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:49:16 by acaraban          #+#    #+#             */
-/*   Updated: 2023/09/15 12:49:17 by acaraban         ###   ########.fr       */
+/*   Updated: 2023/09/15 13:22:19 by acaraban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-
-void init_here_vars(t_here *here)
+void	init_here_vars(t_here *here)
 {
 	here->i = 0;
 	here->r = 0;
@@ -25,7 +24,7 @@ void init_here_vars(t_here *here)
 	here->fd = 0;
 }
 
-void here_condition(t_here *here)
+void	here_condition(t_here *here)
 {
 	if (here->boo)
 	{
@@ -39,8 +38,8 @@ void here_condition(t_here *here)
 
 int	ft_heredoc(char **arr, t_content *cont)
 {
-	t_here *here;
-	
+	t_here	*here;
+
 	here = (t_here *)ft_calloc(sizeof(t_here), 1);
 	init_here_vars(here);
 	while (arr[here->i])
@@ -54,7 +53,8 @@ int	ft_heredoc(char **arr, t_content *cont)
 			return (free_no_val(here, cont), 0);
 		if (pos_char(here->val, '$'))
 		{
-			here->aux = ft_add_varent(here->val, pos_char(here->val, '$'), cont[0].global[0].env, cont);
+			here->aux = ft_add_varent(here->val, \
+			pos_char(here->val, '$'), cont[0].global[0].env, cont);
 			val_is_aux(here);
 		}
 		if (!ft_strcmp(arr[here->i], here->val))
