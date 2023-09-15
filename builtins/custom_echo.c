5ad72fc9f072873c5b88bcd045c775ee74eb20e6
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-15 12:40:04 +0200

    echo -n vacio y con  mas argus

--- builtins/custom_echo.c
+++ builtins/custom_echo.c
@@ -6,7 +6,7 @@
 /*   By: msintas- <msintas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 19:10:37 by msintas-          #+#    #+#             */
-/*   Updated: 2023/09/14 20:54:40 by msintas-         ###   ########.fr       */
+/*   Updated: 2023/09/15 12:38:13 by msintas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,21 @@ void	custom_echo(t_content *cont, int i)
 	int total;
 
 	n = 0;
-	while(cont[i].full_comand[n]) // solo para contar cuantos args me estan pasando
-	{
-		printf("cont[i].full_comand[%d]: %s\n", n, cont[i].full_comand[n]);
+	while(cont[i].full_comand[n])
 		n++;
-	}
 	total = n;
-	n = 2; // pongo el index desde donde quiero imprimir
-	printf("total args = %d\n", n);
+	n = 2;
 	if (cont[i].full_comand[2] == NULL)
-	{
 		exit(EXIT_SUCCESS);
-	}
 	else
 	{
 		while(cont[i].full_comand[n])
 		{
-			
-			//printf("cont[i].full_comand[%d]: %s\n", n, cont[i].full_comand[n]);
-			/*printf("start:\n");
-			printf("n = %d\n", n);
-			printf("total: %d\n", total);*/
-			ft_printf("%s", cont[i].full_comand[n]);
-			if (n < total)
-			{
-				// imprime espacio
-				//printf("dentro");
-				printf(" ");
-			}
+			if (n == 2 || n == total)
+				ft_printf("%s", cont[i].full_comand[n]);
+			else
+				ft_printf(" %s", cont[i].full_comand[n]);
 			n++;
 		}
-		
 	}
 }
