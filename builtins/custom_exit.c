4dd8d8b01f4198ee6ded9c191ba5f2458743914f
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-09-16 13:36:16 +0200

    corrijo fallo del exit

--- builtins/custom_exit.c
+++ builtins/custom_exit.c
@@ -6,7 +6,7 @@
 /*   By: msintas- <msintas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 19:11:19 by msintas-          #+#    #+#             */
-/*   Updated: 2023/09/16 13:11:42 by msintas-         ###   ########.fr       */
+/*   Updated: 2023/09/16 13:21:19 by msintas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,17 @@
     e.g. bash: exit: algo: numeric argument required
 */
 
-void	check_options(t_content *cont, int i)
+int	check_options(t_content *cont, int i)
 {
 	int	n;
 
 	n = 0;
+	if (cont[i].full_comand[2])
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(ft_strjoin(ft_strjoin(cont[i].cmd, ": "), ": too many arguments\n"), 2);
+		return (2);
+	}
 	while (cont[i].full_comand[1][n])
 	{
 		if (cont[i].full_comand[1][0] == '-')
@@ -42,19 +48,17 @@ void	check_options(t_content *cont, int i)
 		}
 		n++;
 	}
+	return (0);
 }
 
 void	custom_exit(t_content *cont, int i, t_pipes *fds)
 {
-	if (cont[i].full_comand[2])
-	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(ft_strjoin(ft_strjoin(cont[i].cmd, ": "), ": too many arguments\n"), 2);
-		return ;
-	}
 	if (cont[i].full_comand[1])
 	{
-		check_options(cont, i);
+		if (check_options(cont, i) == 2)
+		{
+			return ;
+		}
 	}
 	printf("exit\n");
 	ft_free(fds);
